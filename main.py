7726c3b5e2d3bcfd68dd87449892f5f72c33aafa
Jota Farias <fariasjota09@gmail.com>
2023-03-04 11:50:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import datetime
-
-import requests
 import telebot
 
 from keys import TOKEN, verificar_cotacao
@@ -10,43 +7,8 @@ from utils import (
     mensagem_padrao,
     moedas,
     moedas_sigla,
+    ver_cotacao,
 )
-
-
-def ver_cotacao(cot: str):
-    url = "https://economia.awesomeapi.com.br/last/" + cot
-
-    conteudo = requests.get(url).json()
-    lista_conteudo = list(conteudo)
-
-    resposta = ""
-
-    for cont in lista_conteudo:
-        cotacao = conteudo[cont]
-        cotacao_nome = cotacao["name"]
-        cotacao_nome = cotacao_nome.replace("/", " - ").upper()
-        cotacao_compra = float(cotacao["ask"])
-        cotacao_venda = float(cotacao["bid"])
-        cotacao_alta = float(cotacao["high"])
-        cotacao_baixa = float(cotacao["low"])
-        cotacao_variacao_pct = float(cotacao["pctChange"])
-        cotacao_data_horario = datetime.datetime.strptime(
-            cotacao["create_date"], "%Y-%m-%d %H:%M:%S"
-        )
-        cotacao_data_horario = cotacao_data_horario.strftime(
-            "%d/%m/%y %H:%M:%S"
-        )
-
-        resposta += f"{cotacao_nome}\n\n"
-        resposta += f"*Compra*: {cotacao_compra:.4f}\n"
-        resposta += f"*Venda*: {cotacao_venda:.4f}\n\n"
-        resposta += f"*Alta*: {cotacao_alta:.4f}\n"
-        resposta += f"*Baixa*: {cotacao_baixa:.4f}\n\n"
-        resposta += f"*Variação*: {cotacao_variacao_pct:.2f}%\n"
-        resposta += f"*Data/Hora*: {cotacao_data_horario}\n\n"
-
-    return resposta
-
 
 bot = telebot.TeleBot(TOKEN)
 
